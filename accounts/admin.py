39df4f5a5173a02d774e6ad98cda9d81ed208bae
ygag-fayis <mfayisam@gmail.com>
2022-04-26 13:15:55 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from accounts.models import Accounts, Department
+
 # Register your models here.
+
+admin.site.register(Accounts)
+admin.site.register(Department)
